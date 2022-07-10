ddf9771b4dabae4585d2026a701d642956ceb518
iradakhf <iradakhf@code.edu.az>
2022-07-10 14:43:42 +0400

    add all

---
+++ PhotoAlbum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    internal class PhotoAlbum
+    {
+        protected int pages;
+        public PhotoAlbum(int pagesWanted)
+        {
+            this.pages = pagesWanted;
+        }
+        public PhotoAlbum()
+        {
+            pages = 24;
+        }
+        public int GetPhotoAlbumPages()
+        {
+            return pages;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ConsoleApp24
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BigAlbum big = new();
+                Console.WriteLine(big.GetPhotoAlbumPages());
+            PhotoAlbum photo = new(100);
+            Console.WriteLine(photo.GetPhotoAlbumPages());
+            PhotoAlbum pho = new PhotoAlbum();
+            Console.WriteLine(pho.GetPhotoAlbumPages() );
+        }
+    }
+}
